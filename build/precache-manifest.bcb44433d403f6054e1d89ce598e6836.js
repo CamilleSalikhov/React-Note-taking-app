b6a3a1a1e84f68eb0335281c602c85134f6b475e
Camille Salikhov <dontlookbackinanger@yandex.ru>
2019-05-07 23:18:40 +0300

    changes

---
+++ build/precache-manifest.bcb44433d403f6054e1d89ce598e6836.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be52b3b5049dec9485b79c73de91f7f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dd392457fc03c86f07d",
+    "url": "/static/css/main.c07152f8.chunk.css"
+  },
+  {
+    "revision": "9252307b0c0832b5fd44",
+    "url": "/static/js/2.2cfb2f5e.chunk.js"
+  },
+  {
+    "revision": "9dd392457fc03c86f07d",
+    "url": "/static/js/main.f0ce9d33.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
